beebfa90b4fa1c9db69a6213b70c20cdd62aa170
tphien <tranphuochien96@gmail.com>
2017-03-12 21:46:36 +0700

    refactor intent code

--- modules/products/client/controllers/products.client.controller.js
+++ modules/products/client/controllers/products.client.controller.js
@@ -1,54 +1,54 @@
 (function () {
-    'use strict';
+  'use strict';
 
     // Products controller
-    angular
+  angular
         .module('products')
         .controller('ProductsController', ProductsController);
 
-    ProductsController.$inject = ['$scope', '$state', '$window', 'Authentication', 'productResolve'];
+  ProductsController.$inject = ['$scope', '$state', '$window', 'Authentication', 'productResolve'];
 
-    function ProductsController ($scope, $state, $window, Authentication, product) {
-        var vm = this;
+  function ProductsController ($scope, $state, $window, Authentication, product) {
+    var vm = this;
 
-        vm.authentication = Authentication;
-        vm.product = product;
+    vm.authentication = Authentication;
+    vm.product = product;
 
-        vm.error = null;
-        vm.form = {};
-        vm.remove = remove;
-        vm.save = save;
+    vm.error = null;
+    vm.form = {};
+    vm.remove = remove;
+    vm.save = save;
 
         // Remove existing Product
-        function remove() {
-            if ($window.confirm('Are you sure you want to delete?')) {
-                vm.product.$remove($state.go('products.list'));
-            }
-        }
+    function remove() {
+      if ($window.confirm('Are you sure you want to delete?')) {
+        vm.product.$remove($state.go('products.list'));
+      }
+    }
 
         // Save Product
-        function save(isValid) {
-            if (!isValid) {
-                $scope.$broadcast('show-errors-check-validity', 'vm.form.productForm');
-                return false;
-            }
+    function save(isValid) {
+      if (!isValid) {
+        $scope.$broadcast('show-errors-check-validity', 'vm.form.productForm');
+        return false;
+      }
 
             // TODO: move create/update logic to service
-            if (vm.product._id) {
-                vm.product.$update(successCallback, errorCallback);
-            } else {
-                vm.product.$save(successCallback, errorCallback);
-            }
+      if (vm.product._id) {
+        vm.product.$update(successCallback, errorCallback);
+      } else {
+        vm.product.$save(successCallback, errorCallback);
+      }
 
-            function successCallback(res) {
-                $state.go('products.view', {
-                    productId: res._id
-                });
-            }
+      function successCallback(res) {
+        $state.go('products.view', {
+          productId: res._id
+        });
+      }
 
-            function errorCallback(res) {
-                vm.error = res.data.message;
-            }
-        }
+      function errorCallback(res) {
+        vm.error = res.data.message;
+      }
     }
+  }
 }());
